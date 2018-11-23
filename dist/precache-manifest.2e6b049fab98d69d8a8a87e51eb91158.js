1b2778c180710c5d76d076a84b919e1209000545
kdy <kdy@aiitec.com>
2018-11-23 10:14:08 +0800

    commit

---
+++ dist/precache-manifest.2e6b049fab98d69d8a8a87e51eb91158.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5913e41e5750636b1efa",
+    "url": "css/chunk-3ccbd30e.6af8ca07.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "d3484f9b1acbb2956810",
+    "url": "js/chunk-vendors.3e939a40.js"
+  },
+  {
+    "revision": "5913e41e5750636b1efa",
+    "url": "js/chunk-3ccbd30e.7e14e9d3.js"
+  },
+  {
+    "revision": "8f650453dd464fb537b1",
+    "url": "js/app.3714fe43.js"
+  },
+  {
+    "revision": "bf7ddb378aa9f05326a9",
+    "url": "js/Toast.585087a0.js"
+  },
+  {
+    "revision": "bc75b0f7e78ca1044530",
+    "url": "js/HelloWorld.35cf78aa.js"
+  },
+  {
+    "revision": "b9300f78aa553178fe956b75f05424db",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8f650453dd464fb537b1",
+    "url": "css/app.f679c450.css"
+  },
+  {
+    "revision": "bc75b0f7e78ca1044530",
+    "url": "css/HelloWorld.23b82dae.css"
+  }
+];
---
+++ dist/registerServiceWorker.js
@@ -0,0 +1,29 @@
+importScripts("precache-manifest.2e6b049fab98d69d8a8a87e51eb91158.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/* eslint-disable no-console */
+
+import { register } from 'register-service-worker'
+
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready () {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    cached () {
+      console.log('Content has been cached for offline use.')
+    },
+    updated () {
+      console.log('New content is available; please refresh.')
+    },
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
+}
+
