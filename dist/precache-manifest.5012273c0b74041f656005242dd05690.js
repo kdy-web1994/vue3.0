58553d86a765824be142917cd09caa71b8efdd3b
kdy <kdy@aiitec.com>
2018-12-14 11:43:40 +0800

    commit

---
+++ dist/precache-manifest.5012273c0b74041f656005242dd05690.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ba47b1362ad8f307ff",
+    "url": "js/chunk-3ccbd30e.002616d2.js"
+  },
+  {
+    "revision": "41a9be8550ccdc36672fbfa5ee7cbdcf",
+    "url": "skeleton.json"
+  },
+  {
+    "revision": "8a6d6d0b0c5c094186940d607fcd51f0",
+    "url": "index.html"
+  },
+  {
+    "revision": "f290e640fda9bf89b8b0",
+    "url": "js/Toast.49e6edb8.js"
+  },
+  {
+    "revision": "ba6b3341854ce3ace8ce",
+    "url": "js/app.02063067.js"
+  },
+  {
+    "revision": "41a9be8550ccdc36672fbfa5ee7cbdcf",
+    "url": "public/skeleton.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "28997b64938cf5e893b1",
+    "url": "js/HelloWorld.367e3c36.js"
+  },
+  {
+    "revision": "a3cd105fc32f2b723fb1168fc8c22080",
+    "url": "dll/vendor.js"
+  },
+  {
+    "revision": "58122afd071901bfeaa0a9472946ae6d",
+    "url": "dll/vendor-mainfest.json"
+  },
+  {
+    "revision": "42ba47b1362ad8f307ff",
+    "url": "css/chunk-3ccbd30e.e2707afb.css"
+  },
+  {
+    "revision": "ba6b3341854ce3ace8ce",
+    "url": "css/app.f679c450.css"
+  },
+  {
+    "revision": "28997b64938cf5e893b1",
+    "url": "css/HelloWorld.23b82dae.css"
+  }
+];
